4f6b8573dba5659cc468f6d718991a9bb2dc1689
Kaley Main <kmain@switch.tv>
2017-03-14 10:25:44 +1100

    Add test case for issue #430

---
+++ tests/cases/Issue430.tsx
@@ -0,0 +1,11 @@
+// @onlyOwnGrammar - As this has jsx
+const ProductPrice = ({inStock, price}) => {
+  const weekDays = []
+  return (
+    return weekDays.filter((weekDay, index) => ({} || index < 3)).map((weekDay, key) => (
+      <p key={key}>
+        
+      </p>
+    ))
+  )
+}
